f5ec63749750a43b984fa27d6bcbe1b7b52f30be
Marco Munizaga <git@marcopolo.io>
2020-05-03 18:19:47 -0700

    Add base

---
+++ base-configuration.nix
@@ -0,0 +1,167 @@
+# Edit this configuration file to define what should be installed on
+# your system.  Help is available in the configuration.nix(5) man page
+# and in the NixOS manual (accessible by running ‘nixos-help’).
+
+{ config, pkgs, ... }:
+
+let pureZsh = pkgs.callPackage ./pkgs/pure-zsh.nix { };
+in {
+  imports = [ # Include the results of the hardware scan.
+    ./hardware-configuration.nix
+    ./qemu.nix
+    <home-manager/nixos>
+  ];
+
+  nixpkgs.config.allowUnfree = true;
+
+  nix.trustedUsers = [ "root" "marco" ];
+
+  # Use the systemd-boot EFI boot loader.
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.efi.canTouchEfiVariables = true;
+
+  # networking.hostName = "nixos"; # Define your hostname.
+  # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
+
+  # The global useDHCP flag is deprecated, therefore explicitly set to false here.
+  # Per-interface useDHCP will be mandatory in the future, so this generated config
+  # replicates the default behaviour.
+  networking.useDHCP = false;
+  networking.interfaces.wlp3s0.useDHCP = true;
+  networking.hostName = "rex"; # Define your hostname.
+  networking.wireless.enable =
+    true; # Enables wireless support via wpa_supplicant.
+  networking.wireless.networks = {
+    "07d931_5g" = { psk = "***REMOVED***"; };
+  };
+  networking.wireless.interfaces = [ "wlp3s0" ];
+
+  security.sudo.wheelNeedsPassword = false;
+
+  # Set your time zone.
+  time.timeZone = "America/Los_Angeles";
+
+  # List packages installed in system profile. To search, run:
+  # $ nix search wget
+  environment.systemPackages = with pkgs; [ wget vim git pureZsh zsh ];
+
+  # Configure network proxy if necessary
+  # networking.proxy.default = "http://user:password@proxy:port/";
+  # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
+
+  # Select internationalisation properties.
+  # i18n.defaultLocale = "en_US.UTF-8";
+  # console = {
+  #   font = "Lat2-Terminus16";
+  #   keyMap = "us";
+  # };
+
+  # Set your time zone.
+  # time.timeZone = "Europe/Amsterdam";
+
+  # List packages installed in system profile. To search, run:
+  # $ nix search wget
+  # environment.systemPackages = with pkgs; [
+  #   wget vim
+  # ];
+
+  # Some programs need SUID wrappers, can be configured further or are
+  # started in user sessions.
+  # programs.mtr.enable = true;
+  programs.gnupg.agent = {
+    enable = true;
+    enableSSHSupport = true;
+    pinentryFlavor = "gnome3";
+  };
+
+  # List services that you want to enable:
+
+  # Enable the OpenSSH daemon.
+  # services.openssh.enable = true;
+
+  # Open ports in the firewall.
+  # networking.firewall.allowedTCPPorts = [ ... ];
+  # networking.firewall.allowedUDPPorts = [ ... ];
+  # Or disable the firewall altogether.
+  # networking.firewall.enable = false;
+
+  # Enable CUPS to print documents.
+  # services.printing.enable = true;
+
+  # Enable sound.
+  # sound.enable = true;
+  # hardware.pulseaudio.enable = true;
+
+  # Enable the X11 windowing system.
+  services.xserver.enable = true;
+  services.xserver.layout = "us";
+  # Enable touchpad support.
+  services.xserver.libinput.enable = true;
+  # Enable the KDE Desktop Environment.
+  services.xserver.displayManager.sddm.enable = true;
+  services.xserver.desktopManager.plasma5.enable = true;
+
+  services.illum.enable = true;
+  services.logind.lidSwitchExternalPower = "ignore";
+  services.logind.lidSwitch = "ignore";
+
+  services.openssh.enable = true;
+  services.avahi = {
+    enable = true;
+    publish.domain = true;
+    publish.addresses = true;
+    publish.enable = true;
+    publish.userServices = true;
+    nssmdns = true;
+  };
+
+  # Allow aarch64 emulation with qemu
+  qemu-user = {
+    arm = true;
+    aarch64 = true;
+  };
+
+  services.k3s.enable = true;
+
+  virtualisation.libvirtd.enable = true;
+  users.users.marco = {
+    isNormalUser = true;
+    shell = pkgs.zsh;
+    extraGroups = [ "wheel" "libvirtd" ]; # Enable ‘sudo’ for the user.
+    initialPassword = "password";
+    createHome = true;
+    openssh.authorizedKeys.keys = [
+      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIPs7qqifLNNLNvjBKmsgTefmxLO0tstGBfZ4BXv3KmDn marcomunizaga@marco-mbp"
+    ];
+  };
+  programs.zsh.enable = true;
+
+  programs.command-not-found.enable = false;
+  programs.bash.interactiveShellInit = ''
+    source ${pkgs.nix-index}/etc/profile.d/command-not-found.sh
+  '';
+
+  # system.activationScripts.extraUserActivation.text = ''
+  #   ln -sfn /etc/per-user/alacritty ~/.config/
+  #   mkdir -p ~/.zfunctions
+  #   ln -sfn ${pureZshPrompt}/pure.zsh ~/.zfunctions/prompt_pure_setup
+  #   ln -sfn ${pureZshPrompt}/async.zsh ~/.zfunctions/async
+  # '';
+  home-manager.users.marco = import ./home/home.nix;
+
+  # Pull configuration from github every 5 minutes
+  services.cron = {
+    enable = true;
+    systemCronJobs = [ "*/5 * * * *      root    date >> /tmp/cron.log" ];
+  };
+
+  # This value determines the NixOS release from which the default
+  # settings for stateful data, like file locations and database versions
+  # on your system were taken. It‘s perfectly fine and recommended to leave
+  # this value at the release version of the first install of this system.
+  # Before changing this value read the documentation for this option
+  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
+  system.stateVersion = "20.03"; # Did you read the comment?
+
+}
+
