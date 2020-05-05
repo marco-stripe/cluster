ba9937dcb1e059021e870eb10440a472fe4f5913
Marco Munizaga <git@marcopolo.io>
2020-05-04 22:02:46 -0700

    k3s on pi

--- pi4.nix
+++ pi4.nix
@@ -1,8 +1,12 @@
 { config, pkgs, ... }: {
   networking.hostName = "pi4"; # Define your hostname.
-  imports = [ # Include the results of the hardware scan.
+  imports = [
     ./base-configuration.nix
+    ./nixpkgs/nixos/modules/services/cluster/k3s
   ];
+
+  # Use k3s from the latest nixpkgs, but otherwise keep a stable system
+  nixpkgs = { overlays = [ (import ./overlays/k3s) ]; };
 
   networking.interfaces.wlan0.useDHCP = true;
   networking.wireless.interfaces = [ "wlan0" ];
@@ -19,4 +23,6 @@
       };
     };
   };
+
+  services.k3s.enable = true;
 }
