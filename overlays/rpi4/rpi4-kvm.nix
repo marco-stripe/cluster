cb1a6da44cb9fd6ac34af503a6c997d33f34fa5c
Marco Munizaga <git@marcopolo.io>
2020-05-12 10:33:03 -0700

    Tweaks

--- overlays/rpi4/rpi4-kvm.nix
+++ overlays/rpi4/rpi4-kvm.nix
@@ -1,11 +1,36 @@
 self: super: {
-  linux_rpi4_kvm = super.linuxManualConfig {
-    inherit (super) stdenv hostPlatform;
-    inherit (linux_rpi4) src;
-    version = "${linux_rpi4.version}-kvm";
+  linux_rpi4_kvm_bak = super.linuxManualConfig {
+    inherit (super) stdenv;
+    inherit (super.linux_rpi4) src;
+    version = "${super.linux_rpi4.version}-kvm";
 
-    configfile = ../rpi4_kvm.config;
+    configfile = ./rpi4_kvm.config;
     allowImportFromDerivation = true;
   };
+  linux_rpi4_kvm = super.linux_rpi4.override ({
+    name = "linux_rpi4_kvm";
+    extraConfig = ''
+      CONFIG_HAVE_KVM_IRQCHIP y
+      CONFIG_HAVE_KVM_IRQFD y
+      CONFIG_HAVE_KVM_IRQ_ROUTING y
+      CONFIG_HAVE_KVM_EVENTFD y
+      CONFIG_KVM_MMIO y
+      CONFIG_HAVE_KVM_MSI y
+      CONFIG_HAVE_KVM_CPU_RELAX_INTERCEPT y
+      CONFIG_KVM_VFIO y
+      CONFIG_HAVE_KVM_ARCH_TLB_FLUSH_ALL y
+      CONFIG_KVM_GENERIC_DIRTYLOG_READ_PROTECT y
+      CONFIG_HAVE_KVM_IRQ_BYPASS y
+      CONFIG_HAVE_KVM_VCPU_RUN_PID_CHANGE y
+      CONFIG_IRQ_BYPASS_MANAGER y
+      CONFIG_VIRTUALIZATION y
+      CONFIG_KVM y
+      CONFIG_KVM_ARM_HOST y
+      CONFIG_KVM_INDIRECT_VECTORS y
+      CONFIG_VHOST_CROSS_ENDIAN_LEGACY y
+      CONFIG_PREEMPT_NOTIFIERS y
+      CONFIG_MMU_NOTIFIER y
+    '';
+  });
   linuxPackages_rpi4_kvm = super.linuxPackagesFor self.linux_rpi4_kvm;
 }
--- pi4_kvm.nix
+++ pi4_kvm.nix
@@ -1,10 +1,13 @@
 { config, pkgs, ... }: {
   networking.hostName = "pi4"; # Define your hostname.
-  imports =
-    [ ./base-configuration.nix ./nixpkgs/nixos/modules/services/cluster/k3s ];
+  imports = [ ./base-configuration.nix ];
+  # ./nixpkgs/nixos/modules/services/cluster/k3s ];
 
   # Use k3s from the latest nixpkgs, but otherwise keep a stable system
-  nixpkgs = { overlays = [ (import ./overlays/k3s) ]; };
+  nixpkgs = {
+    overlays =
+      [ (import ./overlays/k3s) (import ./overlays/rpi4/rpi4-kvm.nix) ];
+  };
 
   networking.interfaces.wlan0.useDHCP = true;
   networking.wireless.interfaces = [ "wlan0" ];
@@ -22,7 +25,7 @@
     };
   };
 
-  services.k3s.enable = true;
+  # services.k3s.enable = true;
 
   networking.wireless.enable =
     true; # Enables wireless support via wpa_supplicant.
