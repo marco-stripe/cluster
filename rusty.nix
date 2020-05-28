4292b52cac5900d52d4e2077431efb720029b1d3
Marco Munizaga <git@marcopolo.io>
2020-05-27 18:15:20 -0700

    Tweak

--- rusty.nix
+++ rusty.nix
@@ -4,6 +4,7 @@
   imports = [ # Include the results of the hardware scan.
     ./base-configuration.nix
     ./modules/qemu.nix
+    <nixpkgs/nixos/modules/virtualisation/google-compute-image.nix>
   ];
 
   # Allow aarch64 emulation with qemu
