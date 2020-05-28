947e776cfd82545f5edaeb1e9530d51c008ae83d
Marco Munizaga <git@marcopolo.io>
2020-05-27 18:16:15 -0700

    Tweak

--- rusty.nix
+++ rusty.nix
@@ -2,9 +2,9 @@
   networking.hostName = "rusty"; # Define your hostname.
   networking.wireless.enable = false;
   imports = [ # Include the results of the hardware scan.
-    ./base-configuration.nix
-    ./modules/qemu.nix
     <nixpkgs/nixos/modules/virtualisation/google-compute-image.nix>
+    ./modules/qemu.nix
+    ./base-configuration.nix
   ];
 
   # Allow aarch64 emulation with qemu
