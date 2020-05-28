629e652d5a9496f4d72607b45827a081ff58cd2d
Marco Munizaga <git@marcopolo.io>
2020-05-28 07:28:10 -0700

    Add wg

--- rusty.nix
+++ rusty.nix
@@ -55,6 +55,52 @@
     ];
   };
 
+  networking.nat.enable = true;
+  networking.nat.externalInterface = "eth0";
+  networking.nat.internalInterfaces = [ "wg0" ];
+  networking.firewall = {
+    # Handled by GCP
+    # allowedUDPPorts = [ 51820 ];
+
+    # This allows the wireguard server to route your traffic to the internet and hence be like a VPN
+    # For this to work you have to set the dnsserver IP of your router (or dnsserver of choice) in your clients
+    extraCommands = ''
+      iptables -t nat -A POSTROUTING -s 12.10.0.0/24 -o eth0 -j MASQUERADE
+    '';
+  };
+
+  networking.wireguard.interfaces = {
+    # "wg0" is the network interface name. You can name the interface arbitrarily.
+    wg0 = {
+      # Determines the IP address and subnet of the server's end of the tunnel interface.
+      ips = [ "12.10.0.1/24" ];
+
+      # The port that Wireguard listens to. Must be accessible by the client.
+      listenPort = 51820;
+
+      # Path to the private key file.
+      #
+      # Note: The private key can also be included inline via the privateKey option,
+      # but this makes the private key world-readable; thus, using privateKeyFile is
+      # recommended.
+      privateKeyFile = "/root/wireguard-keys/private";
+
+      peers = [
+        # List of allowed peers.
+        { # Feel free to give a meaning full name
+          # Public key of the peer (not a file path).
+          publicKey = "cU5OU0iEu05l0SXF0salb7CkkT16a7k3wA1CpAZMMk4";
+          # List of IPs assigned to this peer within the tunnel subnet. Used to configure routing.
+          allowedIPs = [ "12.10.0.2/32" ];
+        }
+        # { # John Doe
+        #   publicKey = "{john doe's public key}";
+        #   allowedIPs = [ "10.100.0.3/32" ];
+        # }
+      ];
+    };
+  };
+
   # This value determines the NixOS release from which the default
   # settings for stateful data, like file locations and database versions
   # on your system were taken. It‘s perfectly fine and recommended to leave
