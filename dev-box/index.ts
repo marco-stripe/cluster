148137184201c80dfcf6d62b109b562c2bf3accd
Marco Munizaga <git@marcopolo.io>
2020-06-23 19:46:39 -0700

    Tweak

--- dev-box/index.ts
+++ dev-box/index.ts
@@ -4,15 +4,6 @@ import * as aws from "@pulumi/aws";
 const main = pulumi.output(aws.getCallerIdentity({ async: true }));
 export const accountID = main.accountId
 
-const nixArm2003 = pulumi.output(accountID.apply(accountID => aws.getAmi({
-  filters: [{
-    name: "name",
-    values: ["NixOS-20.03beta-81915.gfedcba-aarch64-linux"],
-  }],
-  owners: [accountID], // This owner ID is Amazon
-  mostRecent: true,
-})));
-
 const group = new aws.ec2.SecurityGroup("remoteBuilder-allowSSH", {
   ingress: [
     { protocol: "tcp", fromPort: 22, toPort: 22, cidrBlocks: ["0.0.0.0/0"] },
@@ -24,34 +15,18 @@ const group = new aws.ec2.SecurityGroup("remoteBuilder-allowSSH", {
   ]
 });
 
-const server = new aws.ec2.Instance("remote-builder1", {
-  // @ts-ignore - a real instance type I swear!
-  instanceType: "a1.metal",
+const server = new aws.ec2.SpotInstanceRequest("dev-box", {
+  instanceType: "r5a.4xlarge",
   securityGroups: [group.name], // reference the security group resource above
-  ami: nixArm2003.id,
+  ami: "ami-06562f78dca68eda2", // NixOS 20.03
   rootBlockDevice: {
-    volumeSize: 256
+    volumeSize: 64 // TODO make this smaller
   },
   keyName: "fruit_fig2"
-});
+})
 
 export const instanceID = server.id
-
-const localNixpkgs = "$HOME/nixpkgs"
-
-// Build the pi4 kernel with KVM enabld
-// Breaking down the command a bit:
-// -j0 means no local workers. We want to make sure to do this all on the server
-// the builders arg is explained here: https://nixos.org/nix/manual/#chap-distributed-builds
-export const nixBuildPi4KVM = server.publicIp.apply((publicIP: string) => `\
-nix-build -j0 '<nixpkgs/nixos>' \
-  -A system \
-  -I nixos-config=../pi4_kvm.nix \
-  -I nixpkgs=${localNixpkgs} \
-  -I nixpkgs-overlays=../overlays/rpi4 \
-  --argstr system aarch64-linux \
-  --builders 'ssh://root@${publicIP} aarch64-linux - 16 2 kvm,nixos-test,benchmark,big-parallel'
-`)
+export const serverIP = server.publicIp
 
 const region = pulumi.runtime.getConfig("aws:region")!
 const stopAction = accountID.apply(accountID => `arn:aws:swf:${region}:${accountID}:action/actions/AWS_EC2.InstanceId.Terminate/1.0`)
@@ -59,7 +34,7 @@ const stopAction = accountID.apply(accountID => `arn:aws:swf:${region}:${account
 const minute = 60
 const period = 2 * minute
 
-const metricAlarm = new aws.cloudwatch.MetricAlarm("sleepingBuilder", {
+const metricAlarm = new aws.cloudwatch.MetricAlarm("sleepingDevBox", {
   comparisonOperator: "LessThanOrEqualToThreshold",
   evaluationPeriods: 60 * minute / period,
   metricName: "CPUUtilization",
